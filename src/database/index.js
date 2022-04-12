5bbabc0c3c93a8581dbcf2ab8e1420389c1b11fb
FrostTaco <puppyloganpuppy@gmail.com>
2022-04-12 11:17:42 -0400

    Added support for DATABASE_URL

--- src/database/index.js
+++ src/database/index.js
@@ -1,20 +1,12 @@
-const { Sequelize } = require('sequelize');
-const fs = require('fs');
-const { path } = require('../utils/fs');
-const types = require('./dialects');
+const { Sequelize } = require("sequelize");
+const fs = require("fs");
+const { path } = require("../utils/fs");
+const types = require("./dialects");
 
-module.exports = async client => {
+module.exports = async (client) => {
+	const { DB_TYPE, DATABASE_URL } = process.env;
 
-	const {
-		DB_TYPE,
-		DB_HOST,
-		DB_PORT,
-		DB_USER,
-		DB_PASS,
-		DB_NAME
-	} = process.env;
-
-	const type = (DB_TYPE || 'sqlite').toLowerCase();
+	const type = (DB_TYPE || "sqlite").toLowerCase();
 
 	const supported = Object.keys(types);
 	if (!supported.includes(type)) {
@@ -23,45 +15,42 @@ module.exports = async client => {
 	}
 
 	try {
-		types[type].packages.forEach(pkg => require(pkg));
+		types[type].packages.forEach((pkg) => require(pkg));
 	} catch {
-		const required = types[type].packages.map(i => `"${i}"`).join(' and ');
+		const required = types[type].packages.map((i) => `"${i}"`).join(" and ");
 		client.log.error(new Error(`Please install the package(s) for your selected database type: ${required}`));
 		return process.exit();
 	}
 
 	let sequelize;
 
-	if (type === 'sqlite') {
-		client.log.info('Using SQLite storage');
+	if (type === "sqlite") {
+		client.log.info("Using SQLite storage");
 		sequelize = new Sequelize({
 			dialect: types[type].dialect,
-			logging: text => client.log.debug(text),
-			storage: path('./user/database.sqlite')
+			logging: (text) => client.log.debug(text),
+			storage: path("./user/database.sqlite"),
 		});
 		client.config.defaults.log_messages = false;
-		client.log.warn('Message logging is disabled due to insufficient database');
+		client.log.warn("Message logging is disabled due to insufficient database");
 	} else {
 		client.log.info(`Connecting to ${types[type].name} database...`);
-		sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
+		sequelize = new Sequelize(DATABASE_URL, {
 			dialect: types[type].dialect,
-			host: DB_HOST,
-			logging: text => client.log.debug(text),
-			port: DB_PORT
+			logging: (text) => client.log.debug(text),
 		});
 	}
 
 	try {
 		await sequelize.authenticate();
-		client.log.success('Connected to database successfully');
+		client.log.success("Connected to database successfully");
 	} catch (error) {
-		client.log.warn('Failed to connect to database');
+		client.log.warn("Failed to connect to database");
 		client.log.error(error);
 		return process.exit();
 	}
 
-	const models = fs.readdirSync(path('./src/database/models'))
-		.filter(filename => filename.endsWith('.model.js'));
+	const models = fs.readdirSync(path("./src/database/models")).filter((filename) => filename.endsWith(".model.js"));
 
 	for (const model of models) {
 		require(`./models/${model}`)(client, sequelize);
